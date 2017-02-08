3b09e4ec70146219e7fc66d5c59ff93ed5c2412e
MPPilon <MPPilon@gmail.com>
2017-02-08 12:04:53 -0500

    Completed the doGet override method of the account servlet

--- src/java/servlet/AccountServlet.java
+++ src/java/servlet/AccountServlet.java
@@ -16,13 +16,31 @@
 
 package servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Account;
 
 /**
  * Provides an Account Balance and Basic Withdrawal/Deposit Operations
  */
 @WebServlet("/account")
 public class AccountServlet extends HttpServlet {
+    Account account = new Account();
     
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        try (PrintWriter out = response.getWriter()) {
+            double balance = account.getBalance();
+            response.setHeader("Cache-Control", "private, no-store, no-cache, must-revalidate");
+            response.setHeader("Pragma", "no-cache");
+            response.setDateHeader("Expires", 0);
+            out.println(balance);
+        } catch (IOException ex) {
+            System.err.println("Something went wrong: " + ex.getMessage());
+        }
+    }
 }
